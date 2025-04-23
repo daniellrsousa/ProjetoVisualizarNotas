acc5627e8a94962291a407dbff0ec50882e1c477
Liberato <157623242+Liberatinho@users.noreply.github.com>
2025-04-23 20:15:31 -0300

    Adicionando Home, Página de login e página de erro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ErroLoginComponent } from './pages/erro-login/erro-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'erro', component: ErroLoginComponent },
+  { path: '**', redirectTo: '' } // fallback
+];
---
+++ src/app/pages/erro-login/erro-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-erro-login',
+  imports: [RouterLink],
+  templateUrl: './erro-login.component.html',
+  styleUrl: './erro-login.component.css'
+})
+export class ErroLoginComponent {
+
+}
